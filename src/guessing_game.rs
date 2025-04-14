060d525f13229ee0cec39fbe4d5e31afa3194cc3
Mortal-Man <mwachalamwachala@gmail.com>
2025-04-14 21:45:52 +0300

    generics

---
+++ src/guessing_game.rs
@@ -0,0 +1,49 @@
+use rand::prelude::*;
+use std::io;
+use std::cmp::Ordering;
+
+fn guessing_game(){
+    println!("Pick any two numbers between 1 and 6, if their sum is greater than the numbers i pick, you win a million dollards");
+    let mut x = String::new();
+    let mut y = String::new();
+
+    println!("The first number is...");
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Horrors");
+
+    println!("The second is...");
+    io::stdin()
+        .read_line(&mut y)
+        .expect("A Minooooooooor");
+    let choices = vec![
+        x.trim().parse::<i32>().expect("Hey Now"),
+        y.trim().parse::<i32>().expect("Say now")
+    ];
+
+    let mut machine = rand::rng();
+    let mut die:Vec<i32> = (1..=6).collect();
+
+    
+    let mut roll:Vec<i32> = Vec::new();
+    roll.push(*die.choose(&mut machine).expect("one"));
+    roll.push(*die.choose(&mut machine).expect("two"));
+
+    // let roll = vec![
+    //     die.choose(&mut machine).expect("one"),
+    //     die.choose(&mut machine).expect("two")
+    // ];
+
+    let rollsum = roll.iter().sum::<i32>();
+    let choicesum = choices.iter().sum::<i32>();
+    println!("The sum of your choices is {:?}", choicesum);
+
+    match rollsum.cmp(&choicesum){
+        Ordering::Less => println!("Too High! The sum of my die is {:?}, less than your {:?}", rollsum, choicesum),
+        Ordering::Greater =>println!("Too Low! The sum of my die is {:?}, greater than your {:?}", rollsum, choicesum),
+        Ordering::Equal =>println!("Great! {:?} is equal to {:?}", rollsum, choicesum),
+    }
+   
+    
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,30 @@
-use rand::prelude::*;
+use std::collections::HashMap;
 use std::io;
-use std::cmp::Ordering;
+
+
+
 
 fn main(){
-    println!("Pick any two numbers between 1 and 6, if their sum is greater than the numbers i pick, you win a million dollards");
-    let mut x = String::new();
-    let mut y = String::new();
-
-    println!("The first number is...");
-    io::stdin()
-        .read_line(&mut x)
-        .expect("Horrors");
-
-    println!("The second is...");
-    io::stdin()
-        .read_line(&mut y)
-        .expect("A Minooooooooor");
-    let choices = vec![
-        x.trim().parse::<i32>().expect("Hey Now"),
-        y.trim().parse::<i32>().expect("Say now")
-    ];
-
-    let mut machine = rand::rng();
-    let mut die:Vec<i32> = (1..=6).collect();
-
-    
-    let mut roll:Vec<i32> = Vec::new();
-    roll.push(*die.choose(&mut machine).expect("one"));
-    roll.push(*die.choose(&mut machine).expect("two"));
-
-    // let roll = vec![
-    //     die.choose(&mut machine).expect("one"),
-    //     die.choose(&mut machine).expect("two")
-    // ];
-
-    let rollsum = roll.iter().sum::<i32>();
-    let choicesum = choices.iter().sum::<i32>();
-    println!("The sum of your choices is {:?}", choicesum);
-
-    match rollsum.cmp(&choicesum){
-        Ordering::Less => println!("Too High! The sum of my die is {:?}, less than your {:?}", rollsum, choicesum),
-        Ordering::Greater =>println!("Too Low! The sum of my die is {:?}, greater than your {:?}", rollsum, choicesum),
-        Ordering::Equal =>println!("Great! {:?} is equal to {:?}", rollsum, choicesum),
-    }
+    //generics and traits
    
-    
+    let char_container:Vec<char> = "abcdefghijklmnop".chars().collect();
+    let num_container:Vec<i32> = (1..20).collect();
 
+    let app =generic_largest(&char_container);
+    let app2 = generic_largest(&num_container);
+
+
+
+}
+
+
+fn generic_largest<T:std::fmt::Display+std::cmp::PartialOrd>(param:&[T]) ->&T{
+    let mut largest = &param[0];
+    for item in param{
+        if item > largest{
+            println!("{item} is larger than {largest}");
+            largest = item;
+        }
+    }
+    largest
 }
